89e56e81d03826ca7a4cce1dc0725fc180d16b8e
Roman Shevyakov <roman@mgstr.ru>
2023-02-21 10:26:13 +0300

    feat: deploy the solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessing = 1;
+        this.min = 0;
+        this.max = 2;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessing = Math.round(this.min + (this.max - this.min) / 2);
+        return this.guessing;
     }
 
     lower() {
-
+        this.max = this.guessing;
     }
 
     greater() {
-
+        this.min = this.guessing;
     }
 }
 
